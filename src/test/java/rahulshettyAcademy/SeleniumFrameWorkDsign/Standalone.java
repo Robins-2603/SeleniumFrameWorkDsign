d417f18bad7dfb02ee811f8b3204b29dcd7f65b2
Robins <robinsforwork@gmail.com>
2026-09-07 19:03:09 +0530

    checking the toast container and animation after clicking on Add to cart

--- src/test/java/rahulshettyAcademy/SeleniumFrameWorkDsign/Standalone.java
+++ src/test/java/rahulshettyAcademy/SeleniumFrameWorkDsign/Standalone.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 // we will start writing the selenium code here afterwards we will convert it into framework standards.
 
@@ -39,6 +41,21 @@ public class Standalone {
 		
 		zara.stream().forEach(s->s.findElement(By.xpath(".//button[@class = 'btn w-10 rounded']")).click());
 
+		/*
+		WebElement zara = products.stream().filter(s->s.findElement(By.xpath("//b[contains(text(),'ZARA')]")).getText().equalsIgnoreCase("ZARA COAT 3")).findFirst().orElse(null);
+		
+		System.out.println(zara.getText());
+		*/
+		
+		// checking the toast container and animation after clicking on "Add to cart"
+		//using explicit wait to check this toast container
+		
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		
+		//verifying the Item in Cart
+		driver.findElement(By.cssSelector("button[routerlink='/dashboard/cart']")).click();
+		
 	}
 
 }
